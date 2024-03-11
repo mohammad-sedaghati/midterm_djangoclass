0f1eb32cfdd200d795500d30cb0be0b860063d03
01 <ali.sedaaghati@gmail.com>
2024-03-11 20:22:23 +0330

    header and footer html created

--- midterm_mft201/home_module/views.py
+++ midterm_mft201/home_module/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def home_page(request):
+    return render(request, '_layout.html', {
+        # 'slides': slides,
+        # 'products': products
+    })
