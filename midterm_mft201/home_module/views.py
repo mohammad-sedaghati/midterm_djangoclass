3fc3f576f7ac5e52c4dfb7296d38a98c0097322a
01 <ali.sedaaghati@gmail.com>
2024-03-13 16:38:08 +0330

    The main house page building was built

--- midterm_mft201/home_module/views.py
+++ midterm_mft201/home_module/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-
+from product_module.models import Products
 
 # Create your views here.
+
 def home_page(request):
-    return render(request, '_layout.html', {
+    return render(request, 'home_page.html', {
         # 'slides': slides,
         # 'products': products
     })
